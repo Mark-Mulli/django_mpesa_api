5977d789fcc70e3f68c5ba6c9d214232f973c591
Mark Mulli <markmulli001@gmail.com>
2023-11-19 10:10:20 +0300

    integrate mpesa api

--- main/views.py
+++ main/views.py
@@ -1,14 +1,42 @@
+import requests
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from main import mpesa
+from main.serializer import PaymentSerializer
 
 
 # Create your views here.
 @api_view(["POST"])
 def initiate_payment(request):
+    serializer = PaymentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        phone = serializer.validated_data["phone"]
+        amount = serializer.validated_data["amount"]
+        headers = mpesa.generate_request_headers()
+        data = {
+            "BusinessShortCode": mpesa.get_business_shortcode(),
+            "Password": mpesa.generate_password(),
+            "Timestamp": mpesa.get_current_timestamp(),
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": phone,
+            "PartyB": mpesa.get_business_shortcode(),
+            "PhoneNumber": phone,
+            "CallBackURL": mpesa.get_callback_url(),
+            "AccountReference": "Sth unique",
+            "TransactionDesc": "Payment for services"
+        }
 
-    return Response({"message":"Initiated STK PUSH"})
+        resp = requests.post(mpesa.get_payment_url(), json=data, headers=headers)
+        print("RESPONSE FROM SAF ", resp.json())
+
+        return Response({"message": "Initiated STK PUSH"})
 
 
+@api_view(["POST"])
 def handle_callback(request):
-    return None
+    data = request.data
+    print("CALLBACK FROM SAF", data)
+    return Response({"message": "Received callback"})
--- mpesa_stk_push/settings.py
+++ mpesa_stk_push/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-^66a7v&!v1+)at@7t2djqk6_(_bom3g-02l)5=t)@e7o(ek6%p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mpesa_stk_push.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -102,18 +98,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -124,3 +118,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MPESA_API = {
+    "BIZ_SHORT_CODE": "xxxx",
+    "CALLBACK_URL": " xxxx",
+    "PAYMENT_URL": "xxxx",
+    "CREDENTIALS_URL": "xxxx",
+    "CONSUMER_KEY": "xxxx",
+    "CONSUMER_SECRET": "xxxx",
+    "PASS_KEY": "xxxx",
+}
